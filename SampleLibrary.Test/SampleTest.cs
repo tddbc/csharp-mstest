d68042b493c5fd9700223b72be514b880ba085eb
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-06-10 22:49:53 +0900

    create sample test project

---
+++ SampleLibrary.Test/SampleTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleLibrary.Test
+{
+  [TestClass]
+  public class SampleTest
+  {
+    [TestMethod]
+    public void TestSay()
+    {
+      var sample = new Sample();
+      Assert.AreEqual("Hello TDD Boot Camp!", sample.Say());
+    }
+  }
+}
